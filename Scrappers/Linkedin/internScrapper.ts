270f63b6ec6d6bb3ac0dccf9c71d2d9ad1777b7f
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-04-16 08:49:12 +0200

    :zap: scrapper for interns, and push the jobs to DB

---
+++ Scrappers/Linkedin/internScrapper.ts
@@ -0,0 +1,76 @@
+import { LinkedinScraper, events, experienceLevelFilter, onSiteOrRemoteFilter, typeFilter } from "linkedin-jobs-scraper";
+import { ScraperOptions, deafultScraperOptions, queryTitle } from "./linkedinScrapping";
+import { dataJob } from "../../types/modules";
+import { managedata } from "../../bin/manageData";
+
+
+
+const jobs:dataJob[] = [];
+export interface type{
+  Type:types
+}
+export enum types{
+  INTERNSHIP='Internship',
+  ENTRYLEVEL='EntryLevel'
+}
+
+export async function internScrapping(){
+    const intern = new LinkedinScraper(deafultScraperOptions);
+    const options = ScraperOptions
+    
+      const filters = {
+        type: [typeFilter.FULL_TIME, typeFilter.CONTRACT, typeFilter.INTERNSHIP, typeFilter.PART_TIME],
+        onSiteOrRemote: [onSiteOrRemoteFilter.REMOTE, onSiteOrRemoteFilter.HYBRID, onSiteOrRemoteFilter.ON_SITE], // This will override global options [onSiteOrRemoteFilter.ON_SITE],
+        // experience: [experienceLevelFilter.INTERNSHIP, experienceLevelFilter.ENTRY_LEVEL],
+      }
+    
+      const queries = queryTitle.map((q) => {
+        return (
+          {
+            query: q,
+            options: {
+              ...options,
+              filters: {
+                ...filters,
+                experience: [experienceLevelFilter.INTERNSHIP],
+              }
+            }
+          }
+        )
+      })
+
+    
+      intern.on(events.scraper.data, (data) => {
+        jobs.push({
+          id: data.jobId,
+          title: data.title,
+          company: data.company,
+          link: data.applyLink ? data.applyLink : data.link,
+          logo: data.companyImgLink || '',
+          deadline: data.date,
+          skills: '',
+          type: types.INTERNSHIP,
+        });
+      });
+
+    //   intern.on(events.scraper.metrics, (metrics) => {
+    //     console.log(`Processed=${metrics.processed}`, `Failed=${metrics.failed}`, `Missed=${metrics.missed}`);
+    //   });
+    
+    //   intern.on(events.scraper.error, (err) => {
+    //     console.error(err);
+    //   });
+    
+      intern.on(events.scraper.end, () => {
+        console.log('Internship has been done!');
+        // console.log(jobs);
+        const sendToDB = new managedata(jobs)
+      });
+    
+      // Run queries concurrently    
+      await Promise.all([
+        intern.run(queries),
+      ])
+      // Close browser
+      await intern.close();
+}
