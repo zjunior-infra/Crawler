ab7be9b555cbae58598ca27a7c61e78846083b77
Hamed Osama <hamedosama19@gmail.com>
2023-05-07 08:00:49 +0200

    add validateQuery

--- scrappers/Wuzzuf/wuzzufScrapping.ts
+++ scrappers/Wuzzuf/wuzzufScrapping.ts
@@ -1,14 +1,14 @@
-const Countries = {
+const CountriesFilter = {
   Egypt: 'Egypt',
   UnitedArabEmirates: 'United Arab Emirates',
   SaudiArabia: 'Saudi Arabia',
 }
-const Cities = {
+const CitiesFilter = {
   Cairo: 'Cairo',
   Alexandria: 'Alexandria',
   Giza: 'Giza',
 }
-const Areas = {
+const AreasFilter = {
   NasrCity: 'Nasr City',
   Maadi: 'Maadi',
   Heliopolis: 'Heliopolis',
@@ -18,12 +18,12 @@ const Areas = {
   Mohandseen: 'Mohandessin',
   Downtown: 'Downtown',
 }
-const CareerLevels = {
+const CareerLevelsFilter = {
   Student: 'Student',
   EntryLevel: 'Entry Level',
   Experienced: 'Experienced',
 }
-const JobTypes = {
+const JobTypesFilter = {
   FullTime: 'Full time',
   PartTime: 'Part time',
   Internship: 'Internship',
@@ -44,19 +44,107 @@ interface IQuery {
   options?: IFilters
 }
 
-
-
-const setFilters = (filters: IQuery) => {
-
+interface IQueryValidationError {
+  param: string
+  reason: string
 }
 
-setFilters({
+const validateQuery = (filters: IQuery): IQueryValidationError[] => {
+  const errors: IQueryValidationError[] = [];
+
+  // query must be string
+  if (filters.query && typeof (filters.query) !== "string") {
+    errors.push({
+      param: "query",
+      reason: `Must be a string`
+    });
+  }
+
+  if (filters.options) {
+    let {
+      country,
+      city,
+      area,
+      career_level,
+      job_types,
+    } = filters.options;
+
+    // country must be string or array of strings
+    if (country) {
+      const allowed = Object.values(CountriesFilter)
+      if (!Array.isArray(country))
+        country = [country];
+
+      if (!country.every(e => allowed.includes(e))) {
+        errors.push({
+          param: "options.country",
+          reason: `Must be option of ${allowed.join(', ')}`
+        });
+      }
+
+    }
+
+    // city must be string or array of strings
+    if (city) {
+      const allowed = Object.values(CitiesFilter)
+      if (!Array.isArray(city))
+        city = [city];
+
+      if (!city.every(e => allowed.includes(e))) {
+        errors.push({
+          param: "options.city",
+          reason: `Must be option of ${allowed.join(', ')}`
+        });
+      }
+    }
+
+    // area must be string or array of strings
+    if (area) {
+      const allowed = Object.values(AreasFilter)
+      if (!Array.isArray(area))
+        area = [area];
+      if (!area.every(e => allowed.includes(e))) {
+        errors.push({
+          param: "options.area",
+          reason: `Must be option of ${allowed.join(', ')}`
+        });
+      }
+    }
+
+    // career_level must be string
+    if (career_level) {
+      const allowed = Object.values(CareerLevelsFilter)
+      if (!allowed.includes(career_level)) {
+        errors.push({
+          param: "options.career_level",
+          reason: `Must be option of ${allowed.join(', ')}`
+        });
+      }
+    }
+
+    // job_types must be string
+    if (job_types) {
+      const allowed = Object.values(JobTypesFilter)
+      if (!allowed.includes(job_types)) {
+        errors.push({
+          param: "options.job_types",
+          reason: `Must be option of ${allowed.join(', ')}`
+        });
+      }
+    }
+
+
+  }
+  console.log(errors)
+  return errors;
+}
+validateQuery({
   query: 'software engineer',
   options: {
-    country: [Countries.Egypt],
-    city: [Cities.Cairo],
-    area: [Areas.Maadi],
-    career_level: CareerLevels.EntryLevel,
-    job_types: JobTypes.FullTime,
+    country: [CountriesFilter.Egypt],
+    city: [CitiesFilter.Cairo],
+    area: [AreasFilter.Maadi],
+    career_level: CareerLevelsFilter.EntryLevel,
+    job_types: JobTypesFilter.FullTime,
   }
 })
