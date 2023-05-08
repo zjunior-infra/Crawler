3079d1ce71ac84e9bbf0a86ab084550c25389f3c
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-05-08 19:39:15 +0200

    comment all tests, to have working script

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import { LinkedinScrapper } from './scrappers/Linkedin/linkedinScrapping'
 import { IQuery } from './types/modules'
 import { scrape } from './utils/pupt/scraper'
-import { searchQuery } from './utils/pupt/searchQuery'
 import { uuidJob } from './utils/pupt/uuid-wuzzuf'
 
-// (async ()=>{
-//     await LinkedinScrapper()
-// })()
+(async ()=>{
+    await LinkedinScrapper()
+})()
 
 // console.log(searchQuery({search:'backend'}))
-scrape();
+// scrape('front-end');
+// scrape('backend')
+// scrape()
