ee7a362187e6ec574c9e8bac492b3e4df4b150a5
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-08-09 02:06:31 +0300

    read the args to run a spcific type of scrapper

--- index.ts
+++ index.ts
@@ -1,12 +1,34 @@
+import { entryScrapping } from './scrappers/Linkedin/entryScrapper'
+import { internScrapping } from './scrappers/Linkedin/internScrapper'
 import { LinkedinScrapper } from './scrappers/Linkedin/linkedinScrapping'
 import { IQuery } from './types/modules'
 import { scrape } from './utils/pupt/scraper'
 import { uuidJob } from './utils/pupt/uuid-wuzzuf'
 
+const scrapper = process.argv.slice(2).toString()
+switch (scrapper.toLowerCase()){
+    case 'intern':
+        (async ()=>{
+                await internScrapping()
+            })()
+        break;
+    case 'entry':
+        (async ()=>{
+            await entryScrapping()
+        })()
+        break;
+    default:
+        (async ()=>{
+            await LinkedinScrapper()
+        })()
+}
 
-(async ()=>{
-    await LinkedinScrapper()
-})()
+
+
+
+// (async ()=>{
+//     await LinkedinScrapper()
+// })()
 
 // console.log(searchQuery({search:'backend'}))
 // scrape('front-end');
